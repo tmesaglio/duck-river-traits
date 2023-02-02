5217f05717cab4802c913367e9027561471988bf
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2023-02-02 17:19:17 +1100

    updates

--- R/missing species from AusTraits.R
+++ R/missing species from AusTraits.R
@@ -14,4 +14,41 @@ distinct_aus <- distinct_aus %>%
 
 x <- setdiff(distinct_sl, distinct_aus) 
 
-write_csv(x,"data/austraits_missing_species.csv")
+
+#install austraits
+remotes::install_github("traitecoevo/austraits", dependencies = TRUE, upgrade = "ask")
+
+library(austraits) 
+austraits <- load_austraits(version = "4.1.0", path = "austraits")
+
+#get traits
+
+spvector <- sl$taxon_name
+splist<-dplyr::select(sl, taxon_name) 
+
+#all field adult records
+(austraits %>% join_all)$traits %>%
+  filter(taxon_name %in% spvector) %>%
+  filter(trait_name == "plant_height") %>%
+  filter(life_stage=="adult" & 
+           basis_of_record %in% c("field","literature","literature, field","preserved_specimen") & #preserved specimen is flora data
+           value_type=="maximum") -> trait1
+
+
+# mean and max height by species 
+trait1 %>%         
+  group_by(taxon_name) %>%
+  summarize(max_max_height=max(as.numeric(value),na.rm=T),mean_max_height=mean(as.numeric(value),na.rm=T)) -> trait1a
+
+
+check<-data.frame(trait1$taxon_name)
+
+check<-check %>% 
+  rename(
+    species = trait1.taxon_name
+  )
+
+y <- setdiff(distinct_aus, check) 
+
+write_csv(y,"data/austraits_missing_species.csv")
+write_csv(x,"data/austraits_missing_species2.csv")
