b3f0071215ebddd481cee1a894756563490ca191
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2023-02-07 07:53:57 +1100

    Update missing species from AusTraits.R

--- R/missing species from AusTraits.R
+++ R/missing species from AusTraits.R
@@ -4,12 +4,12 @@ library(dplyr)
 sl <- read_csv("species_lists.csv")
 aus_sl <- read_csv("duck_creek_floras_data.csv")
 
-distinct_sl = sl %>% distinct(species)
+distinct_sl = sl %>% distinct(taxon_name)
 distinct_aus = aus_sl %>% distinct(taxon_name)
 
 distinct_aus <- distinct_aus %>% 
   rename(
-    species = taxon_name
+    taxon_name = species
   )
 
 x <- setdiff(distinct_sl, distinct_aus) 
